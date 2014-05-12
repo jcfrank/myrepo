f00e8bcadd3d71ed8ab797073a744f5d3d9648ea
frankts_chen <jcfrank7@gmail.com>
2014-05-12 18:20:07 +0800

    Remove redundant 'repo' in WrapperPath()

--- wrapper.py
+++ wrapper.py
@@ -20,7 +20,8 @@ import os
 
 
 def WrapperPath():
-  return os.path.join(os.path.dirname(__file__), 'repo')
+  #return os.path.join(os.path.dirname(__file__), 'repo')
+  return os.path.join(os.path.dirname(__file__), '')
 
 _wrapper_module = None
 def Wrapper():
